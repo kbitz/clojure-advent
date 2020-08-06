33aec5a7d28ee78db69485084b1fa965a7a394c9
kb <kb@wardbitz.com>
2020-08-06 00:08:35 -0400

    completed ex4

---
+++ src/clojure_advent/ex4.clj
@@ -0,0 +1,31 @@
+(ns clojure-advent.ex3
+  (:gen-class))
+
+(def input (range 128392 (inc 643281)))
+
+(defn has-six-digits? [n]
+  (= 6 (count (str n))))
+
+(defn has-adjacent-pair? [n]
+  (false? (empty? (filter (fn [[a b]] (= a b)) (partition 2 1 (str n))))))
+
+(defn not-decreasing? [n]
+  (apply <= (map #(Integer/parseInt %) (vec (map str (vec (str n)))))))
+
+(defn has-iso-adjacent-pair? [n]
+  (if (false? (has-adjacent-pair? n))
+    false
+    (let [pairs (filter (fn [[a b]] (= a b)) (partition 2 1 (str n)))]
+      (false? (empty? (filter #(= 1 %) (for [p pairs] (count (filter #(= p %) pairs)))))))))
+
+(defn part-1 []
+  (count (filter (every-pred
+                  has-six-digits?
+                  has-adjacent-pair?
+                  not-decreasing?) input)))
+
+(defn part-2 []
+  (count (filter (every-pred
+                  has-six-digits?
+                  has-iso-adjacent-pair?
+                  not-decreasing?) input)))
